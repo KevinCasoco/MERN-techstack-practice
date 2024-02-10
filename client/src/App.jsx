c931e21b122c497ba08469c8c8dd554b02e5d855
KevinCasoco <andreikevincasoco@gmail.com>
2024-02-10 08:51:20 +0800

    clieny side routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './Signup'
 import Login from './Login'
+import Dashboard from './Dashboard'
+import Profile from './Profile'
+import ForgotPassword from './ForgotPassword'
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/register' element={<Signup />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/forgot-password' element={<ForgotPassword />}></Route>
       </Routes>
     </BrowserRouter>
   )
